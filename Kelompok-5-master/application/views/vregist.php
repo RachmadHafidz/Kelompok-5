6ff91c7eeec6110cb964bf0b1c5fda78b28b5135
bayuilham <55471288+bayuilham@users.noreply.github.com>
2020-04-26 02:44:45 +0700

    lupa pass

---
+++ Kelompok-5-master/application/views/vregist.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Register</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="css/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="css/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="css/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-regist100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+					<span class="login100-form-title">
+						REGISTER
+					</span>
+                    
+                    <form action="/action_page.php" class="was-validated">
+    <div class="row">
+      <div class="col col-md-12">
+        <label for="uname">Nama</label>
+        <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+        <br>
+        <label for="uname">Nomor Telepon</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+        <br>
+        <label for="uname">Nomor Identitas</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+        <br>
+        <label for="uname">Foto</label>
+        <input type="file" class="form-control" placeholder="Enter password" name="pswd">
+        <br>
+        <label for="uname">Email</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+        <br>
+        <label for="uname">Username</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+        <br>
+        <label for="uname">Password</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+      </div>
+    </div>
+    <br>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember" required> I agree on blabla.
+        <div class="valid-feedback">Valid.</div>
+        <div class="invalid-feedback">Check this checkbox to continue.</div>
+      </label>
+    </div>
+   
+  </form>
+
+<div class="flex-col-c p-t-50 p-b-40">
+<button type="submit" class="btn btn-primary mt-3">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+<!--===============================================================================================-->
+	<script src="css/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="css/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="css/vendor/bootstrap/js/popper.js"></script>
+	<script src="css/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="css/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="css/vendor/daterangepicker/moment.min.js"></script>
+	<script src="css/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="css/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="css/js/main.js"></script>
+
+</body>
+</html>
