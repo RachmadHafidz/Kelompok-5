f8166a70d628cb910d6a2646ae89f6d0a70fc395
chikitachika <chikitachika17@gmail.com>
2020-06-30 11:05:02 +0700

    .

---
+++ android/loginn.php
@@ -0,0 +1,29 @@
+<?php
+
+ if($_SERVER['REQUEST_METHOD']=='POST'){
+
+ include 'koneksi.php';
+ 
+ $koneksi = mysqli_connect($HostName,$HostUser,$HostPass,$DatabseName);
+ 
+ $email = $_POST['email'];
+ $password = md5($_POST['password']);
+ 
+ $Sql_Query = "select * from notaris where email = '$email' and password = '$password' ";
+ 
+ $prosescek = mysqli_fetch_array(mysqli_query($koneksi,$Sql_Query));
+ 
+ if(isset($prosescek)){
+ 
+ echo "Data Matched";
+ }
+ else{
+ echo "Invalid Email or Password Please Try Again !";
+ }
+ 
+ }else{
+ echo "Check Again";
+ }
+
+
+?>
