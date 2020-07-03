8108311cfb2b630eae5b0f97cd3f8088d84f096c
RachmadHafidz <55471196+RachmadHafidz@users.noreply.github.com>
2020-07-03 21:51:17 +0700

    Update index.php

--- application/views/menu/index.php
+++ application/views/menu/index.php
@@ -17,7 +17,7 @@
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Menu123</th>
+                        <th scope="col">Menu</th>
                         <th scope="col">Action</th>
 
                     </tr>
